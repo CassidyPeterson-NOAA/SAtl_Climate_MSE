88d5292e751acd418a87ff50f9822a07beb67871
CassidyPeterson-NOAA <cassidy.peterson@noaa.gov>
2022-11-30 17:01:40 -0500

    clean ordering, built worm plots, built cumulative violinplots, plotting par

--- Analyze_Results.R
+++ Analyze_Results.R
@@ -56,18 +56,18 @@ EpiMDeal<-function(data){
 
 ## BUILD FUNCTIONS ##
 # Get Results collated into one list (BSB)
-GetResults<-function(species=species, sp=sp, oScenarios=orderedScenarios){
-  if(grepl(species, pattern="Over")){ # get files of matching species name (correcting for Over results)
-    files1<-list.files(set.file, pattern=species)
+GetResults<-function(species1=species, sp1=sp, oScenarios=orderedScenarios){
+  if(grepl(species1, pattern="Over")){ # get files of matching species name (correcting for Over results)
+    files1<-list.files(set.file, pattern=species1)
   } else {
-    files1<-grep(list.files(set.file, pattern=species), pattern="Over", invert=TRUE, value=TRUE) #
+    files1<-grep(list.files(set.file, pattern=species1), pattern="Over", invert=TRUE, value=TRUE) #
   }
 
   temp_res<-list() # create empty list with sp title.
   for(f in 1:length(files1)){ # get results for each OM
-    lab1 <- gsub("MSE_|\\..*", "", files1[f]); lab2<- gsub(species, sp, lab1); lab3<-gsub(paste0(sp,"_"),"",lab2) # get rds label
+    lab1 <- gsub("MSE_|\\..*", "", files1[f]); lab2<- gsub(species1, sp1, lab1); lab3<-gsub(paste0(sp1,"_"),"",lab2) # get rds label
     assign(lab2, readRDS(file.path(set.file,files1[f]))) # get results for each OM
-    temp_res[lab3]<-get(lab2) # # save OM results in a single object titled sp
+    temp_res[[lab3]]<-get(lab2) # # save OM results in a single object titled sp
   } # end for loop
   temp_res<-temp_res[oScenarios]
   # assign(sp, temp_res)
@@ -192,19 +192,23 @@ Reorder_MPs<-function(PMs_obj, oMPs=orderedMPs){
 
 #### PLOTTING FUNCTIONS ####
 ### trajectories ###
-par(mfrow=c(1,1), mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
+# par(mfrow=c(1,1), mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
 ## one plot each -- new color
 Plot_SSBtraj<-function( fsh, scenarios=NULL, save.png=F,
-                        oMPs=orderedMPs, oScenarios=orderedScenarios,
+                        oMPs=orderedMPs, oScenarios=NULL,
                         colsR=MP_R_col, namesR=MP_namesR_leg){
 
-  data<<-get(fsh)
+  data<-get(fsh)
   if(is.null(scenarios)){
-    scenarios<-names(data)[oScenarios]
+    if(!is.null(oScenarios)){
+      scenarios<-names(data)[oScenarios]
+    } else {
+        scenarios<-names(data)
+        } # end if-else ; define scenarios
   } # end get scenarios
 
   for(res in scenarios){
-    results<<-get(res,data)
+    results<-get(res,data)
 
     MP_names<-data[[1]]@MPs
     MP_namesR<-c(MP_names[oMPs])
@@ -221,9 +225,9 @@ Plot_SSBtraj<-function( fsh, scenarios=NULL, save.png=F,
         pointsize = 24,
         res = 300
       )
+      par(mfrow=c(1,1), mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
     } # end save.png
 
-    # par(mfrow=c(1,1), mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
     max_y<-0
     for(ii in 1:dim(results@SB_SBMSY)[2]){
       max_y<-max(max_y, apply(results@SB_SBMSY[,ii,], 2, median) )
@@ -235,29 +239,37 @@ Plot_SSBtraj<-function( fsh, scenarios=NULL, save.png=F,
       iorder<-which(MP_namesR==iname)
       lines(apply(results@SB_SBMSY[,i,], 2, median), type='l', lwd=2, lty=iorder, col=colsR[iorder])
     }
-    legend("bottom", namesR, lwd=2, lty=1:length(namesR), col=colsR, bty='n', ncol=5, cex=0.75)
-    mtext( paste0(fsh, " ", res), side=3, line=-1)
+    if(save.png==T) legend("bottom", namesR, lwd=2, lty=1:length(namesR), col=colsR, bty='n', ncol=5, cex=0.75)
+    mtext( paste0(fsh, " ", res), side=3, line=-1.2)
 
     if(save.png==T){
       dev.off()
     } # end save.png
 
   } # end for res
+
+  plot(0, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = '', xlab = '')
+  legend("center", namesR, lwd=2, lty=1:length(namesR), col=colsR, bty='n', ncol=2, cex=0.75)
+
 } # end function
 
 
 
 Plot_Catchtraj<-function( fsh, scenarios=NULL, save.png=F,
-                          oMPs=orderedMPs, oScenarios=orderedScenarios,
+                          oMPs=orderedMPs, oScenarios=NULL,
                           colsR=MP_R_col, namesR=MP_namesR_leg){
 
-  data<<-get(fsh)
+  data<-get(fsh)
   if(is.null(scenarios)){
-    scenarios<-names(data)[oScenarios]
+    if(!is.null(oScenarios)){
+      scenarios<-names(data)[oScenarios]
+    } else {
+      scenarios<-names(data)
+    } # end if-else ; define scenarios
   } # end get scenarios
 
   for(res in scenarios){
-    results<<-get(res,data)
+    results<-get(res,data)
 
     MP_names<-data[[1]]@MPs
     MP_namesR<-c(MP_names[oMPs])
@@ -274,9 +286,9 @@ Plot_Catchtraj<-function( fsh, scenarios=NULL, save.png=F,
         pointsize = 24,
         res = 300
       )
+      par(mfrow=c(1,1), mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
     } # end save.png
 
-    # par(mfrow=c(1,1), mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
     max_y<-0
     for(ii in 1:dim(results@Catch)[2]){
       max_y<-max(max_y, apply(results@Catch[,ii,], 2, median) )
@@ -288,46 +300,73 @@ Plot_Catchtraj<-function( fsh, scenarios=NULL, save.png=F,
       iorder<-which(MP_namesR==iname)
       lines(apply(results@Catch[,i,], 2, median), type='l', lwd=2, lty=iorder, col=colsR[iorder])
     }
-    legend("bottom", namesR, lwd=2, lty=1:length(namesR), col=colsR, bty='n', ncol=5, cex=0.75)
-    mtext( paste0(fsh, " ", res), side=3, line=-1)
+    if(save.png==T) legend("bottom", namesR, lwd=2, lty=1:length(namesR), col=colsR, bty='n', ncol=5, cex=0.75)
+    mtext( paste0(fsh, " ", res), side=3, line=-1.2)
 
     if(save.png==T){
       dev.off()
     } # end save.png
 
   } # end for res
+
+  plot(0, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = '', xlab = '')
+  legend("center", namesR, lwd=2, lty=1:length(namesR), col=colsR, bty='n', ncol=2, cex=0.75)
+
 } # end function
 
 
 ## VIOLIN PLOTS ##
-Plot_trelSSB<-function(SPP, ylims=c(NULL)){
+Plot_trelSSB<-function(SPP, ylims=c(NULL), MPnam=MP_namesR_leg, MPcol=MP_R_col){
   par(mfrow=c(4,3))
   for(ii in 1:length(names(SPP))){
-    vioplot(P100(SPP[[ii]], Yrs=-1)@Stat, col=c('grey30','gray40','gray50','gray60','gray70','deepskyblue4','deepskyblue3','deepskyblue','lightskyblue1','lightskyblue'), names=MP_namesR_leg, ylim=ylims); abline(h=1)
+    vioplot(P100(SPP[[ii]], Yrs=-1)@Stat, col=MPcol, names=MPnam, ylim=ylims); abline(h=1)
     mtext(names(SPP)[ii], 3, line=-1.2)
     mtext("SSB ratio", 2, line=1.1)
   }# end for loop
 }
-Plot_PNOF<-function(SPP, ylims=c(NULL), refline=NULL){
+Plot_PNOF<-function(SPP, ylims=c(NULL), refline=NULL, MPnam=MP_namesR_leg, MPcol=MP_R_col){
   par(mfrow=c(4,3))
   for(ii in 1:length(names(SPP))){
-    vioplot(PNOF(SPP[[ii]])@Prob, col=c('grey30','gray40','gray50','gray60','gray70','deepskyblue4','deepskyblue3','deepskyblue','lightskyblue1','lightskyblue'), names=MP_namesR_leg, ylim=ylims); abline(h=1); abline(h=refline, lty=2)
+    vioplot(PNOF(SPP[[ii]])@Prob, col=MPcol, names=MPnam, ylim=ylims); abline(h=1); abline(h=refline, lty=2)
     mtext(names(SPP)[ii], 3, line=-1.2)
     mtext("PNOF", 2, line=1.1)
   }# end for loop
 }
-Plot_P100<-function(SPP, ylims=c(NULL), refline=NULL){
+Plot_P100<-function(SPP, ylims=c(NULL), refline=NULL, MPnam=MP_namesR_leg, MPcol=MP_R_col){
   par(mfrow=c(4,3))
   for(ii in 1:length(names(SPP))){
-    vioplot(P100(SPP[[ii]],Yrs=-10)@Prob, col=c('grey30','gray40','gray50','gray60','gray70','deepskyblue4','deepskyblue3','deepskyblue','lightskyblue1','lightskyblue'), names=MP_namesR_leg, ylim=ylims); abline(h=1); abline(h=refline, lty=2)
+    vioplot(P100(SPP[[ii]],Yrs=-10)@Prob, col=MPcol, names=MPnam, ylim=ylims); abline(h=1); abline(h=refline, lty=2)
     mtext(names(SPP)[ii], 3, line=-1.2)
     mtext("Prob SSB_40-50>SSBMSY", 2, line=1.1)
   }# end for loop
 }
+Plot_AAVY<-function(SPP, ylims=c(NULL), refline=NULL, MPnam=MP_namesR_leg, MPcol=MP_R_col){
+  par(mfrow=c(4,3))
+  for(ii in 1:length(names(SPP))){
+    vioplot(AAVY(SPP[[ii]],Yrs=-10)@Stat, col=MPcol, names=MPnam, ylim=ylims); abline(h=refline, lty=2)
+    mtext(names(SPP)[ii], 3, line=-1.2)
+    mtext("AAVY", 2, line=1.1)
+  }# end for loop
+}
+
+# cumulative violin plots (over all OMs)
+PlotCumPM<-function(sp, title=NULL, Pstat=c('AAVY','trelSSB','trelF','tyield','cyield','PNOF','P100'),
+                    ylims=c(NULL), refline=NULL, MPnam=MP_namesR_leg, MPcol=MP_R_col){
+  dat<-get(paste0(sp,"_PM"))
+  datStat<-get(Pstat, dat)
+
+  vioplot(datStat, col=MPcol, names=MPnam, ylim=ylims); abline(h=refline, lty=2)
+  if(is.null(title)) title<-sp
+  mtext(title, 3, line=-1.2)
+  mtext(Pstat, 2, line=1.1)
+}
+
+
 
 # -------------------------------------------------- GET DATA ------------------------------------------------------
 
-## INPUTS ##
+
+##### INPUTS ######
 set.file<-"MSE_obj/"
 species<-"BlackSeaBass"; sp<-"BSB"
 ## DATA AND PLOTTING INPUTS
@@ -337,41 +376,132 @@ scenarios<-c("base","age0M_hi","age0M_lo","epiM","recdev_hi","recdev_lo",
              "recns","refbias_hi","refbias_lo","uobs_hi","uobs_lo")
 MP_namesR_leg<-c("SCA1","SCA5_c","SCA10_c","SCA5_p","SCA10_p", "GBtarg","ICI","Irat","IT10","Itarg")
 MP_R_col=c('grey30','gray40','gray50','gray60','gray70','deepskyblue4','deepskyblue3','deepskyblue','lightskyblue1','lightskyblue')
+par.args<-list(mar=c(2.6, 2.6, 0.6, 0.6), mgp=c(1.3, 0.25, 0), tck=-0.01)
+par(par.args)
+
+
 
 
 ### Run FUNCTIONS for input species ##
-assign(sp, GetResults(species=species,sp=sp)) # save GetResults with sp
+assign(sp, GetResults(species1=species,sp1=sp)) # save GetResults with sp
 
 # correct results for EpiM SSB nonsensical results
-sptemp<-get(sp)
-summary(sptemp$epiM@SB_SBMSY)
-assign(sp, EpiMDeal(sptemp))
-summary(get(sp)$epiM@SB_SBMSY)
+sptemp<-get(sp)                       # save BSB as sptemp
+summary(sptemp$epiM@SB_SBMSY)         # see nonsensical sptemp results
+assign(sp, EpiMDeal(sptemp))          # deal with nonsensical results -- resave as BSB
+summary(get(sp)$epiM@SB_SBMSY)        # see updated results
 
 # Get performance metrics and reorder
 assign(paste(sp,"PMs",sep='_'), CollatePMs(sp)) #BSB_PMs
-assign(paste(sp, "PM", sep="_"), Reorder_MPs(get(paste0(sp,"_PMs"))) )
+assign(paste(sp, "PM", sep="_"), Reorder_MPs(get(paste0(sp,"_PMs"))) ) #BSB_PM ## REORDERED PERF METRICS
 
 
 
-############## -----------------------------------------------------------------
+
+
 
 
 
 # --------------------------------------------------- PLOTTING -------------------------------------------------
 
-
 # Plot median Trajectories
-par(mfrow=c(4,3))
+par(mfrow=c(4,3), par.args)
 Plot_SSBtraj(fsh=sp)
-par(mfrow=c(4,3))
+par(mfrow=c(4,3), par.args)
 Plot_Catchtraj(fsh=sp)
 
 
 spec<-get(sp)
 # plot violin plots
 Plot_trelSSB(SPP=spec)
+Plot_PNOF(SPP=spec, refline=0.5, ylims=c(0,1.2))
+Plot_P100(SPP=spec, refline=0.5, ylims=c(0,1.2))
+Plot_AAVY(SPP=spec, refline=0.30, ylims=c(0,1.5))
+Plot_AAVY(SPP=spec, refline=0.30)
 
 
-### NEED TO BUILD A FUNCTION TO REORDER BSB
 
+
+
+## BUILD cumulative violins / boxplots
+
+
+par(mfrow=c(1,1))
+PlotCumPM(sp, Pstat="AAVY", refline=c(0.3), title="Black Sea Bass")
+PlotCumPM(sp, Pstat="PNOF", refline=c(0.5), title="Black Sea Bass")
+PlotCumPM(sp, Pstat="P100", refline=c(0.5), title="Black Sea Bass")
+PlotCumPM(sp, Pstat="trelSSB", ylim=c(0,10), refline=c(1), title="Black Sea Bass")
+PlotCumPM(sp, Pstat="cyield",  refline=c(45), title="Black Sea Bass")
+
+
+
+
+
+## BUILD wormplot
+get(sp)$base@MPs
+MPs=c("SCA_1","GB_target_BSB","myICI_BSB")
+myWorm<-function(sp, OM="base", MPs=NULL,                 ### NOTE: MPs names should match sp$OM@MPs names.
+                 metric=c("SB_SBMSY","F_FMSY","AddInd", "Catch"),
+                 nworms=4, iters=NULL, seed=NULL , byMP=FALSE, ... ){
+  dat<-get(OM, get(sp))
+  if(is.null(seed)) seed<-8675309
+  if(is.null(iters)) iters<-sample(1:dat@nsim, nworms, replace=FALSE)
+
+
+  if(metric=="SB_SBMSY" ){ datmet<-dat@SB_SBMSY}
+  if(metric=="F_FMSY"){ datmet<-dat@F_FMSY}
+  if(metric=="Catch"){ datmet<-dat@Catch}
+
+  if(metric=="SB_SBMSY" | metric=="F_FMSY" | metric=="Catch"){
+
+    if(byMP==F){
+      for(i in 1:nworms){
+        datplot<-datmet[iters[i], , ]
+
+        plot(datplot[which(dat@MPs==MPs[1]),], type='l', lty=1, col=1, lwd=2, #col=which(MPs==m)
+             ylim=c(min(datplot)-0.1*(min(datplot)), max(datplot)+0.1*(max(datplot))),
+             ylab=metric, xlab="Projected Year" ); abline(h=1)
+        for(m in 2:length(MPs) ){
+          lines(datplot[which(dat@MPs==MPs[m]),], lty=m,col=m ,lwd=2)
+        } # end m loop
+
+      } # end i for loop
+      legend("topright",MPs,lty=1:length(MPs), col=1:length(MPs))
+    } # end if byMP==F
+
+
+
+    if(byMP==T){
+      # separate by MP
+      for(m in MPs){
+        datplot<-datmet[iters, which(dat@MPs==m), ]
+        plot(datplot[1,], type='l', lty=1, col=1, lwd=2, #col=which(MPs==m)
+             ylim=c(min(datplot)-0.1*(min(datplot)), max(datplot)+0.1*(max(datplot))),
+             ylab=metric, xlab="Projected Year" ); abline(h=1); mtext(m, 3, line=-1.2)
+        for(i in 2:nworms){
+          lines(datplot[i,], lty=i,col=i ,lwd=2)
+        } # end i loop
+
+      } # end m for loop
+    } # if byMP==T
+
+
+
+  } # if metric == SBSBMSY, FFMSY, Catch
+
+
+
+  ### IF metric=="AddInd" UNDER CONSTRUCTION
+  # if(metric=="AddInd"){
+  #   datmet<-c()
+  #   for(m in MPs){
+  #     datmet<-rbind(datmet, get(m, dat@PPD)@AddInd[iters,1,])
+  #   }# end for m loop
+  # }# end if metric=="AddInd"
+
+  return(list(seed=seed, iters=iters))
+} # end function
+
+par(mfrow=c(1,3))
+myWorm(sp, OM="base", MPs=c("SCA_1","GB_target_BSB","myICI_BSB"),
+       metric="SB_SBMSY")
