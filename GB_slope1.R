6e490d945cbfc1cbb9438b131254033304c683e1
CassidyPeterson-NOAA <cassidy.peterson@noaa.gov>
2022-12-14 16:37:10 -0500

    retune GB_slope1

--- GB_slope1.R
+++ GB_slope1.R
@@ -1,3 +1,137 @@
+
+
+GB_slope_BSB<-function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, delta=0.2,
+                        lambda = 0.9, index="AddInd", indd=1, const=1.0525) # w default lambda=1, c=1 | OLD: lambda=0.7, const=1.05 | NEW: lambda=0.9, const=1.0525
+{
+  dependencies = "Data@Cat, Data@Cref, Data@Iref, Data@Ind, Data@AddInd"
+
+  if(index=="AddInd"){
+    INDEX<-Data@AddInd[,indd,]
+  }
+  if(index=="Ind"){
+    INDEX<-Data@Ind
+  }
+
+  Catrec <- Data@Cat[x, length(Data@Cat[x, ])]
+  ind <- (length(Data@Year) - (yrsmth - 1)):length(Data@Year)
+  I_hist <- INDEX[x, ind]
+  yind <- 1:yrsmth
+  slppar <- summary(lm(log(I_hist) ~ yind))$coefficients[2,1:2]
+  if (reps > 1) {
+    Islp <- rnorm(reps, slppar[1], slppar[2])
+  }
+  else {
+    Islp <- slppar[1]
+  }
+  MuC <- Data@Cat[x, length(Data@Cat[x, ])]
+  Cc <- MSEtool::trlnorm(reps, MuC, Data@CV_Cat[x, 1])
+  TAC <- Cc * (1 + lambda * Islp) * const
+  TAC[TAC > ((1+delta) * Catrec)] <- (1+delta) * Catrec
+  TAC[TAC < ((1-delta) * Catrec)] <- (1-delta) * Catrec
+  TAC <- MSEtool::TACfilter(TAC)
+  if (plot)
+    GB_slope_plot(Data, ind, I_hist, MuC, TAC, Islp)
+  Rec <- new("Rec")
+  Rec@TAC <- TAC
+  Rec
+}
+
+class(GB_slope_BSB)<-"MP"
+
+GB_slope_BSB2<-GB_slope_BSB
+formals(GB_slope_BSB2)$lambda<-0.5
+formals(GB_slope_BSB2)$const<-1.03
+class(GB_slope_BSB2)<-"MP"
+
+
+
+GB_slope_RP<-function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, delta=0.2,
+                       lambda = 1.8, index="AddInd", indd=1, const=1) # w default lambda=1, c=1| lambda=1.8, const=1
+{
+  dependencies = "Data@Cat, Data@Cref, Data@Iref, Data@Ind, Data@AddInd"
+
+  if(index=="AddInd"){
+    INDEX<-Data@AddInd[,indd,]
+  }
+  if(index=="Ind"){
+    INDEX<-Data@Ind
+  }
+
+  Catrec <- Data@Cat[x, length(Data@Cat[x, ])]
+  ind <- (length(Data@Year) - (yrsmth - 1)):length(Data@Year)
+  I_hist <- INDEX[x, ind]
+  yind <- 1:yrsmth
+  slppar <- summary(lm(log(I_hist) ~ yind))$coefficients[2,1:2]
+  if (reps > 1) {
+    Islp <- rnorm(reps, slppar[1], slppar[2])
+  }
+  else {
+    Islp <- slppar[1]
+  }
+  MuC <- Data@Cat[x, length(Data@Cat[x, ])]
+  Cc <- MSEtool::trlnorm(reps, MuC, Data@CV_Cat[x, 1])
+  TAC <- Cc * (1 + lambda * Islp) * const
+  TAC[TAC > ((1+delta) * Catrec)] <- (1+delta) * Catrec
+  TAC[TAC < ((1-delta) * Catrec)] <- (1-delta) * Catrec
+  TAC <- MSEtool::TACfilter(TAC)
+  if (plot)
+    GB_slope_plot(Data, ind, I_hist, MuC, TAC, Islp)
+  Rec <- new("Rec")
+  Rec@TAC <- TAC
+  Rec
+}
+
+class(GB_slope_RP)<-"MP"
+
+
+
+
+
+GB_slope_VS<-function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, delta=0.2,
+                       lambda = 0.9, index="AddInd", indd=1, const=1.045) # w default lambda=1| lambda=0.5, const=1.025 _OR_ lambda=0.9, const=1.045 | lambda=0.8, const=1.0375
+{
+  dependencies = "Data@Cat, Data@Cref, Data@Iref, Data@Ind, Data@AddInd"
+
+  if(index=="AddInd"){
+    INDEX<-Data@AddInd[,indd,]
+  }
+  if(index=="Ind"){
+    INDEX<-Data@Ind
+  }
+
+  Catrec <- Data@Cat[x, length(Data@Cat[x, ])]
+  ind <- (length(Data@Year) - (yrsmth - 1)):length(Data@Year)
+  I_hist <- INDEX[x, ind]
+  yind <- 1:yrsmth
+  slppar <- summary(lm(log(I_hist) ~ yind))$coefficients[2,1:2]
+  if (reps > 1) {
+    Islp <- rnorm(reps, slppar[1], slppar[2])
+  }
+  else {
+    Islp <- slppar[1]
+  }
+  MuC <- Data@Cat[x, length(Data@Cat[x, ])]
+  Cc <- MSEtool::trlnorm(reps, MuC, Data@CV_Cat[x, 1])
+  TAC <- Cc * (1 + lambda * Islp) *const
+  TAC[TAC > ((1+delta) * Catrec)] <- (1+delta) * Catrec
+  TAC[TAC < ((1-delta) * Catrec)] <- (1-delta) * Catrec
+  TAC <- MSEtool::TACfilter(TAC)
+  if (plot)
+    GB_slope_plot(Data, ind, I_hist, MuC, TAC, Islp)
+  Rec <- new("Rec")
+  Rec@TAC <- TAC
+  Rec
+}
+
+class(GB_slope_VS)<-"MP"
+
+GB_slope_VS2<-GB_slope_VS
+formals(GB_slope_VS2)$lambda<-0.5
+formals(GB_slope_VS2)$const<-1.02
+class(GB_slope_VS2)<-"MP"
+
+
+
 # > GB_slope
 # function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, lambda = 1)
 # {
@@ -30,127 +164,3 @@
 #   <environment: namespace:DLMtool>
 #   attr(,"class")
 # [1] "MP"
-
-GB_slope_BSB<-function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, lambda = 0.7, index="AddInd", indd=1, const=1.05) # w default lambda=1, c=1
-{
-  dependencies = "Data@Cat, Data@Cref, Data@Iref, Data@Ind, Data@AddInd"
-
-  if(index=="AddInd"){
-    INDEX<-Data@AddInd[,indd,]
-  }
-  if(index=="Ind"){
-    INDEX<-Data@Ind
-  }
-
-  Catrec <- Data@Cat[x, length(Data@Cat[x, ])]
-  ind <- (length(Data@Year) - (yrsmth - 1)):length(Data@Year)
-  I_hist <- INDEX[x, ind]
-  yind <- 1:yrsmth
-  slppar <- summary(lm(log(I_hist) ~ yind))$coefficients[2,
-                                                         1:2]
-  if (reps > 1) {
-    Islp <- rnorm(reps, slppar[1], slppar[2])
-  }
-  else {
-    Islp <- slppar[1]
-  }
-  MuC <- Data@Cat[x, length(Data@Cat[x, ])]
-  Cc <- MSEtool::trlnorm(reps, MuC, Data@CV_Cat[x, 1])
-  TAC <- Cc * (1 + lambda * Islp) * const
-  TAC[TAC > (1.2 * Catrec)] <- 1.2 * Catrec
-  TAC[TAC < (0.8 * Catrec)] <- 0.8 * Catrec
-  TAC <- MSEtool::TACfilter(TAC)
-  if (plot)
-    GB_slope_plot(Data, ind, I_hist, MuC, TAC, Islp)
-  Rec <- new("Rec")
-  Rec@TAC <- TAC
-  Rec
-}
-
-class(GB_slope_BSB)<-"MP"
-
-
-
-
-
-GB_slope_RP<-function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, lambda = 1.8, index="AddInd", indd=1, const=1) # w default lambda=1, c=1
-{
-  dependencies = "Data@Cat, Data@Cref, Data@Iref, Data@Ind, Data@AddInd"
-
-  if(index=="AddInd"){
-    INDEX<-Data@AddInd[,indd,]
-  }
-  if(index=="Ind"){
-    INDEX<-Data@Ind
-  }
-
-  Catrec <- Data@Cat[x, length(Data@Cat[x, ])]
-  ind <- (length(Data@Year) - (yrsmth - 1)):length(Data@Year)
-  I_hist <- INDEX[x, ind]
-  yind <- 1:yrsmth
-  slppar <- summary(lm(log(I_hist) ~ yind))$coefficients[2,
-                                                         1:2]
-  if (reps > 1) {
-    Islp <- rnorm(reps, slppar[1], slppar[2])
-  }
-  else {
-    Islp <- slppar[1]
-  }
-  MuC <- Data@Cat[x, length(Data@Cat[x, ])]
-  Cc <- MSEtool::trlnorm(reps, MuC, Data@CV_Cat[x, 1])
-  TAC <- Cc * (1 + lambda * Islp) * const
-  TAC[TAC > (1.2 * Catrec)] <- 1.2 * Catrec
-  TAC[TAC < (0.8 * Catrec)] <- 0.8 * Catrec
-  TAC <- MSEtool::TACfilter(TAC)
-  if (plot)
-    GB_slope_plot(Data, ind, I_hist, MuC, TAC, Islp)
-  Rec <- new("Rec")
-  Rec@TAC <- TAC
-  Rec
-}
-
-class(GB_slope_RP)<-"MP"
-
-
-
-
-
-GB_slope_VS<-function (x, Data, reps = 100, plot = FALSE, yrsmth = 5, lambda = 0.01, index="AddInd", ind=1) # w default lambda=1
-{
-  dependencies = "Data@Cat, Data@Cref, Data@Iref, Data@Ind, Data@AddInd"
-
-  if(index=="AddInd"){
-    INDEX<-Data@AddInd[,ind,]
-  }
-  if(index=="Ind"){
-    INDEX<-Data@Ind
-  }
-
-  Catrec <- Data@Cat[x, length(Data@Cat[x, ])]
-  ind <- (length(Data@Year) - (yrsmth - 1)):length(Data@Year)
-  I_hist <- INDEX[x, ind]
-  yind <- 1:yrsmth
-  slppar <- summary(lm(log(I_hist) ~ yind))$coefficients[2,
-                                                         1:2]
-  if (reps > 1) {
-    Islp <- rnorm(reps, slppar[1], slppar[2])
-  }
-  else {
-    Islp <- slppar[1]
-  }
-  MuC <- Data@Cat[x, length(Data@Cat[x, ])]
-  Cc <- MSEtool::trlnorm(reps, MuC, Data@CV_Cat[x, 1])
-  TAC <- Cc * (1 + lambda * Islp)
-  TAC[TAC > (1.2 * Catrec)] <- 1.2 * Catrec
-  TAC[TAC < (0.8 * Catrec)] <- 0.8 * Catrec
-  TAC <- MSEtool::TACfilter(TAC)
-  if (plot)
-    GB_slope_plot(Data, ind, I_hist, MuC, TAC, Islp)
-  Rec <- new("Rec")
-  Rec@TAC <- TAC
-  Rec
-}
-
-class(GB_slope_VS)<-"MP"
-
-
